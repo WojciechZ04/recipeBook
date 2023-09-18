9ceaed5c97bf4a8240cecd247ef71c30e1434e9f
Wojciech Zieliński <wojtekz.6776@gmail.com>
2023-09-18 21:05:47 +0200

    observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,4 @@ import { Component, Input } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  loadedOption = 'recipes';
-  title = 'PROJECT';
-}
+export class AppComponent {}
