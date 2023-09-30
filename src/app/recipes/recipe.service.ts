ff695fe2e7b4cfe92800901b29f8a7f3578c448d
Wojciech Zieliński <wojtekz.6776@gmail.com>
2023-09-30 09:50:21 +0200

    http

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,30 +9,36 @@ export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
   recipeSelected = new Subject<Recipe>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Sandwich',
-      'Simple sandwich',
-      'https://somethingaboutsandwiches.com/wp-content/uploads/2022/04/ham-sandwich.jpg',
-      [
-        new Ingredient('Bread', 2),
-        new Ingredient('Ham', 2),
-        new Ingredient('Mayo', 1)
-      ]
-    ),
-    new Recipe(
-      'Neapolitan Pizza',
-      'Neapolitan pizza, also known as Naples-style pizza, is a style of pizza made with tomatoes and mozzarella cheese',
-      `https://vincenzosplate.com/wp-content/uploads/2022/08/1500x1500-Photo-1_1968-How-to-MAKE-NEAPOLITAN-PIZZA-with-Dry-Yeast-at-Home-Better-than-a-Pizzeria-in-Naples-V1.jpg`,
-      [
-        new Ingredient('Cheese', 1),
-        new Ingredient('Ham', 1),
-        new Ingredient('Pizza Dough', 1)
-      ]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Sandwich',
+  //     'Simple sandwich',
+  //     'https://somethingaboutsandwiches.com/wp-content/uploads/2022/04/ham-sandwich.jpg',
+  //     [
+  //       new Ingredient('Bread', 2),
+  //       new Ingredient('Ham', 2),
+  //       new Ingredient('Mayo', 1),
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Neapolitan Pizza',
+  //     'Neapolitan pizza, also known as Naples-style pizza, is a style of pizza made with tomatoes and mozzarella cheese',
+  //     `https://vincenzosplate.com/wp-content/uploads/2022/08/1500x1500-Photo-1_1968-How-to-MAKE-NEAPOLITAN-PIZZA-with-Dry-Yeast-at-Home-Better-than-a-Pizzeria-in-Naples-V1.jpg`,
+  //     [
+  //       new Ingredient('Cheese', 1),
+  //       new Ingredient('Ham', 1),
+  //       new Ingredient('Pizza Dough', 1),
+  //     ]
+  //   ),
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
@@ -44,20 +50,20 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
-    }
+  }
 
-    addRecipe(recipe: Recipe) {
-      this.recipes.push(recipe);
-      this.recipesChanged.next(this.recipes.slice());
-    }
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
-    updateRecipe(index: number, newRecipe: Recipe) {
-      this.recipes[index] = newRecipe;
-      this.recipesChanged.next(this.recipes.slice());
-    }
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
-    deleteRecipe(index: number) {
-      this.recipes.splice(index, 1);
-      this.recipesChanged.next(this.recipes.slice());
-    }
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
 }
